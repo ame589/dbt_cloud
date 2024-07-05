e9af97273c98b5c5f2c76f799d3e5ec4323ea676
Amedeo Tortora <a.tortora89@gmail.com>
2024-07-05 09:58:37 +0200

    Delete my_dbt_demo directory

--- my_dbt_demo/target/compiled/my_dbt_demo/models/gold/gold_orders.sql
+++
@@ -1,9 +0,0 @@
-WITH gold_orders AS (
-    SELECT
-        COUNT(order_id) AS order_count,
-        1 AS id
-    FROM `catalog-zurich-01`.`zurich_at_silver`.`silver_orders`
-    )
-
-SELECT *
-FROM gold_orders
